3e4958b572ea72bfd72ce0c9f833ce2c1a456491
crazqx <crazqx949@gmail.com>
2023-09-03 17:59:01 +0300

    first commit

---
+++ 1/XO2.py
@@ -0,0 +1,19 @@
+# Работа с пользователем -----------------------------------------
+
+number1 = int(input("Введите первое число"))
+
+number2 = int(input("Введите второе число"))
+
+number1 += 5 # (number1 = number1 + 5)
+
+# Результаты -----------------------------
+print("Result:", number1 + number2)
+print("Result:", number1 - number2)
+print("Result:", number1 / number2)
+print("Result:", number1 * number2)
+print("Result:", number1 ** number2)
+print("Result:", number1 // number2)
+# ----------------------------------------
+
+word = "Hi"
+print(word * 2) # умножение строки
